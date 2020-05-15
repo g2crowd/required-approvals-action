3a42d9e075a7b1cd9d489630ba3b4df6ef55da56
Craig Landry <clandry@g2crowd.com>
2020-05-15 10:49:03 -0600

    Adding logging to debug

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const unsatisfiedRequirements = require('./labels');
 async function run() {
     const inputLabels = JSON.parse(core.getInput('labels'));
 
+    console.log(`Testing labels: ${inputLabels.join(',')}`);
+
     const result = unsatisfiedRequirements(inputLabels);
+
+    console.log(`unsatisfiedRequirements results: ${result.join(',')}`);
+
     if (result.length > 0) {
         core.setFailed(`Jira ticket indicates the following labels are still needed: ${result.join(', ')}`);
     }
