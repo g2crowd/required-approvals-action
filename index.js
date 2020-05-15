dfbbf443706bd2027d6b9c9f161a4fa63f0c2ccb
Craig Landry <clandry@g2crowd.com>
2020-05-15 07:53:29 -0600

    First pass at an action for this.

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const unsatisfiedRequirements = require('labels');
+
+async function run() {
+    const inputLabels = JSON.parse(core.getInput('labels'));
+
+    const result = unsatisfiedRequirements(inputLabels);
+    if (result.length > 0) {
+        core.setFailed(`Jira ticket indicates the following labels are still needed: ${result.join(', ')}`);
+    }
+}
+
+run();
